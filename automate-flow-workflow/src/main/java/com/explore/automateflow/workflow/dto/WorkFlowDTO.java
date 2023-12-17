c54d5f80451d55e0808161804f3fbeafc5b6c917
Satya <ksdlkumar@outlook.com>
2023-12-17 20:32:51 +0530

    Add ActionDTO, TriggerDTO, and WorkFlowDTO classes

---
+++ automate-flow-workflow/src/main/java/com/explore/automateflow/workflow/dto/WorkFlowDTO.java
@@ -0,0 +1,45 @@
+package com.explore.automateflow.workflow.dto;
+
+import java.util.List;
+
+import com.explore.automateflow.workflow.entity.WorkFlow;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkFlowDTO {
+    private String workflowId;
+    private String workflowName;
+    private String workflowDescription;
+    private String userId;
+    private TriggerDTO triggerDTO;
+    private List<ActionDTO> actionDTOs;
+
+    public static WorkFlowDTO fromEntity(WorkFlow entity) {
+        WorkFlowDTO dto = new WorkFlowDTO();
+        dto.setWorkflowId(entity.getWorkflowId());
+        dto.setWorkflowName(entity.getWorkflowName());
+        dto.setWorkflowDescription(entity.getWorkflowDescription());
+        dto.setUserId(entity.getUserId());
+        return dto;
+    }
+
+    public WorkFlow toEntity() {
+        WorkFlow entity = new WorkFlow();
+        entity.setWorkflowId(this.getWorkflowId());
+        entity.setWorkflowName(this.getWorkflowName());
+        entity.setWorkflowDescription(this.getWorkflowDescription());
+        entity.setUserId(this.getUserId());
+        entity.setTriggerId(this.getTriggerDTO().getTriggerId());
+        List<String> actionIds =this.getActionDTOs().stream().map(ActionDTO::getActionId).toList();
+        entity.setActionIds(actionIds);
+        return entity;
+    }
+}
