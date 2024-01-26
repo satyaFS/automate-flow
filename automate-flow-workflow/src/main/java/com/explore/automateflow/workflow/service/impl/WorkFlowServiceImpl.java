7933b888507923bd8cf13466b9d27f96109d1b16
Satya <ksdlkumar@outlook.com>
2024-01-26 15:58:48 +0530

    To execute actions sequentially

--- automate-flow-workflow/src/main/java/com/explore/automateflow/workflow/service/impl/WorkFlowServiceImpl.java
+++ automate-flow-workflow/src/main/java/com/explore/automateflow/workflow/service/impl/WorkFlowServiceImpl.java
@@ -1,9 +1,12 @@
 package com.explore.automateflow.workflow.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.explore.automateflow.workflow.dto.ActionDTO;
 import com.explore.automateflow.workflow.dto.WorkFlowDTO;
 import com.explore.automateflow.workflow.entity.WorkFlow;
 import com.explore.automateflow.workflow.repository.WorkFlowRepository;
@@ -19,7 +22,8 @@ import java.util.List;
 public class WorkFlowServiceImpl implements WorkFlowService {
     private final WorkFlowRepository workFlowRepository;
     private final WebClient webClient;
-
+    
+    private static final Logger logger = LoggerFactory.getLogger(WorkFlowServiceImpl.class);
     public WorkFlowServiceImpl(WorkFlowRepository workFlowRepository, WebClient webClient) {
         this.workFlowRepository = workFlowRepository;
         this.webClient = webClient; 
@@ -74,15 +78,6 @@ public class WorkFlowServiceImpl implements WorkFlowService {
         });
     }
 
-    // @Override
-    // public Mono<Void> executeWorkFlow(String workflowId, JsonNode triggerResponse) {
-    //     return workFlowRepository.findById(workflowId).flatMapMany(workFlow ->
-    //         Flux.fromIterable(workFlow.getActionIds())
-    //             .concatMap(actionId -> getActionUrl(actionId)
-    //                 .flatMap(url -> webClient.post().uri(url).bodyValue(triggerResponse).retrieve().bodyToMono(String.class))
-    //             )
-    //     ).then();
-    // }
     @Override
     public Mono<Void> executeWorkFlow(String workflowId, JsonNode triggerResponse) {
     return workFlowRepository.findById(workflowId)
@@ -91,19 +86,46 @@ public class WorkFlowServiceImpl implements WorkFlowService {
     }
 
     private Mono<JsonNode> executeActionsSequentially(List<String> actionIds, JsonNode previousResponse) {
-        if (actionIds.isEmpty()) {
-            return Mono.just(previousResponse);
-        }
-    
         String actionId = actionIds.get(0);
+        logger.info("prevouResponse {}", previousResponse);
         List<String> remainingActionIds = actionIds.subList(1, actionIds.size());
-    
-        return getActionUrl(actionId)
-            .flatMap(url -> webClient.post().uri(url).bodyValue(previousResponse).retrieve().bodyToMono(JsonNode.class))
-            .flatMap(response -> executeActionsSequentially(remainingActionIds, response));
+        return getAction(actionId)
+                .flatMap(action -> executeAction(action, previousResponse))
+                .flatMap(response -> {
+                    logger.info("response {}", response);
+                    return remainingActionIds.isEmpty() ? Mono.just(previousResponse)
+                            : executeActionsSequentially(remainingActionIds, response);
+                });
     }
 
     private Mono<String> getActionUrl(String actionId) {
         return webClient.get().uri("/action/{actionId}", actionId).retrieve().bodyToMono(String.class);
+    }
+
+    private Mono<ActionDTO> getAction(String actionId) {
+        return webClient.get().uri("http://localhost:8084/actions/{actionId}", actionId).retrieve().bodyToMono(ActionDTO.class);
+    }
+    
+    private Mono<JsonNode> executeAction(ActionDTO actionDTO, JsonNode previousResponse) {
+        String uri = actionDTO.getUrl();
+        Mono<JsonNode> response;
+        switch (actionDTO.getMethod()) {
+            case "GET":
+                response = webClient.get().uri(uri).retrieve().bodyToMono(JsonNode.class);
+                break;
+            case "POST":
+                response = webClient.post().uri(uri).bodyValue(previousResponse).retrieve().bodyToMono(JsonNode.class);
+                break;
+            case "PUT":
+                response = webClient.put().uri(uri).bodyValue(previousResponse).retrieve().bodyToMono(JsonNode.class);
+                break;
+            case "DELETE":
+                response = webClient.delete().uri(uri).retrieve().bodyToMono(JsonNode.class);
+                break;
+            default:
+                response = webClient.post().uri(uri).bodyValue(previousResponse).retrieve().bodyToMono(JsonNode.class);
+                break;
+        }
+        return response;
     }
 }
